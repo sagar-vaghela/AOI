93db2dfe5ecad4d319600d741d3a9a64143eca40
Sagar Vaghela <sagar19vaghela.sv@gmail.com>
2023-01-17 12:17:42 +0530

    Set Pages and Route

---
+++ src/Components/ConfigurationPage/OFDMTab.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function OFDMTab() {
+  return (
+    <div>ConfigurationOFDMTab</div>
+  )
+}
--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,11 +1,29 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import HomePage from "../Components/HomePage/HomePage";
+import RunningOFDMTab from "../Components/RunningConfigurationPage/OFDMTab/OFDMTab";
+import RunningOFDMTabQAMTab from "../Components/RunningConfigurationPage/QAMTab/QAMTab";
+import ManageConfigurationPage from "../Components/ManageConfigurationPage/ManageConfigurationPage";
+import ConfigurationQAMTab from "../Components/ConfigurationPage/QAMTab";
+import ConfigurationOFDMTab from "../Components/ConfigurationPage/OFDMTab";
+import MainTenanacePage from "../Components/System/MaintenancePage/MaintenancePage";
+import SoftwareUpgradePage from "../Components/System/SoftwareUpgradePage/SoftwareUpgradePage";
+import SettingsPage from "../Components/System/SettingsPage/SettingsPage";
+
 
 export default function Routes() {
   return (
     <Router>
       <Route exact path="/" component={HomePage} />
+      <Route exact path="/runningConfiguration/OFDMTab" component={RunningOFDMTab} />
+      <Route exact path="/runningConfiguration/QAMTab" component={RunningOFDMTabQAMTab} />
+      <Route exact path="/manageConfiguration" component={ManageConfigurationPage} />
+      <Route exact path="/configuration/QAMTab" component={ConfigurationQAMTab} />
+      <Route exact path="/configuration/OFDMTab" component={ConfigurationOFDMTab} />
+      <Route exact path="/system/maintenanace" component={MainTenanacePage} />
+      <Route exact path="/system/softwareUpgrade" component={SoftwareUpgradePage} />
+      <Route exact path="/system/settings" component={SettingsPage} />
+
     </Router>
   );
 }
